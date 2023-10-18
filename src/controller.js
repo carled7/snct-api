34381e633f10d4436b3377e136fb65664a8cfe1c
carled7 <carlos_ed03@outlook.com>
2023-10-18 18:25:48 -0300

    Minor fixes

--- src/controller.js
+++ src/controller.js
@@ -1,25 +1,28 @@
 import express from "express";
 import axios from "axios";
 import { coins } from "./coins.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/get-balances", async (req, res) => {
   const balances = await Promise.all(
     coins.map(async (coin) => {
-      const limit = 1000;
+      const tradeAmount = 1000;
 
       const { data } = await axios.get(
         "https://api.binance.com/api/v3/trades",
-        { params: { symbol: coin.code, limit } }
+        { params: { symbol: coin.code, limit: tradeAmount } }
       );
 
-      const momentVariation = ((data[limit - 1].price / data[0].price) - 1) * 100;
+      const momentVariation = (data[tradeAmount - 1].price / data[0].price - 1) * 100;
 
       return {
         coin: coin.name,
-        currentPrice: data[limit - 1].price,
-        momentVariation
+        currentPrice: data[tradeAmount - 1].price,
+        momentVariation,
       };
     })
   );
